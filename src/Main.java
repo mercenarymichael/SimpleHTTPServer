dca4cd9153f8b4e7a1f5f1ddd6715dab08fa4f25
Gréczi-Zsoldos Miklós <mercenary.michael@mailbox.unideb.hu>
2024-03-10 21:59:06 +0100

    build: Added InputStreamReader and BufferedReader to print out the client's GET request

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -8,7 +10,14 @@ public class Main {
         final ServerSocket server = new ServerSocket(8080);
         System.out.println("Listening for connection on port 8080 ....");
         while (true) {
-            final Socket client = server.accept();
+            Socket clientSocket = server.accept();
+            InputStreamReader isr = new InputStreamReader(clientSocket.getInputStream());
+            BufferedReader reader = new BufferedReader(isr);
+            String line = reader.readLine();
+            while(!line.isEmpty()) {
+                System.out.println(line);
+                line = reader.readLine();
+            }
         }
     }
 }
