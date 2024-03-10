8475d3e7004985ab7dbb111e673628d8054923e4
Gréczi-Zsoldos Miklós <mercenary.michael@mailbox.unideb.hu>
2024-03-10 22:04:48 +0100

    build: The client gets todays date after the GET request

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Date;
 
 
 public class Main {
@@ -10,13 +11,19 @@ public class Main {
         final ServerSocket server = new ServerSocket(8080);
         System.out.println("Listening for connection on port 8080 ....");
         while (true) {
-            Socket clientSocket = server.accept();
-            InputStreamReader isr = new InputStreamReader(clientSocket.getInputStream());
-            BufferedReader reader = new BufferedReader(isr);
-            String line = reader.readLine();
-            while(!line.isEmpty()) {
-                System.out.println(line);
-                line = reader.readLine();
+            try (Socket clientSocket = server.accept()) {
+                InputStreamReader isr = new InputStreamReader(clientSocket.getInputStream());
+                BufferedReader reader = new BufferedReader(isr);
+                String line = reader.readLine();
+                while (!line.isEmpty()) {
+                    System.out.println(line);
+                    line = reader.readLine();
+                }
+
+                Date today = new Date();
+                String httpResponse = "HTTP/1.1 200 OK \r\n\r\n" + today;
+                clientSocket.getOutputStream()
+                        .write(httpResponse.getBytes("UTF-8"));
             }
         }
     }
