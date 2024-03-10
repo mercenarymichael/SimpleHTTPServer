9e09377423e857d9ccf847c8ec37b8657de9fddd
Gréczi-Zsoldos Miklós <mercenary.michael@mailbox.unideb.hu>
2024-03-10 21:44:40 +0100

    build: Created the Server socket

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        final ServerSocket server = new ServerSocket(8080);
+        System.out.println("Listening for connection on port 8080 ....");
+        while (true) {
+
+        }
+    }
+}
